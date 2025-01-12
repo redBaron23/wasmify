54201bb4c9988789fffb5bea453af84b0cc6e182
Patricio Toledo <me@patriciotoledo.ar>
2025-01-12 23:12:03 +1100

    dont ignore

---
+++ src/wasm/media_converter_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __wbg_mediaconverter_free: (a: number, b: number) => void;
+export const mediaconverter_new: (a: any) => [number, number, number];
+export const mediaconverter_set_progress_callback: (a: number, b: any) => void;
+export const mediaconverter_convert_image: (a: number, b: number, c: number) => [number, number, number, number];
+export const mediaconverter_convert_video: (a: number, b: number, c: number) => [number, number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
