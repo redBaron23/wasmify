b444df704c18e5837461bdc0bd3823103a7a8717
Patricio Toledo <me@patriciotoledo.ar>
2025-01-12 23:15:52 +1100

    dont ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignorePatterns: ["src/wasm/*"],
+  },
 ];
 
 export default eslintConfig;
